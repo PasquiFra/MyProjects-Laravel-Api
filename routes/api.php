6d92bd8099c217470d4576e4cfad3425d32f1d6e
Your Name <you@example.com>
2024-03-28 16:42:40 +0100

    postman call OK

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+// Route::get('/projects', [ProjectController::class, 'index']);
+// Route::get('/projects/{project}', [ProjectController::class, 'show']);
+// Route::post('/projects', [ProjectController::class, 'store']);
+// Route::delete('/projects', [ProjectController::class, 'destroy']);
+// Route::put('/projects', [ProjectController::class, 'update']);
+//!OPPURE
+//Route::resource('/projects', ProjectController::class);
+//!OPPURE
+Route::apiResource('/projects', ProjectController::class);
